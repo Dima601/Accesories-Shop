5304024fc1f6838b3d0fb03510cda001f3709937
Dima <poluhovicd601@gmail.com>
2024-12-18 18:43:27 +0200

    add cart

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const navLinks = document.querySelector('.navbar ul');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+const cartButtons = document.querySelectorAll('.add-to-cart');
+
+cartButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        alert('Item added to cart!');
+    });
+});
