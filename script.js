be6650c8f174154c14d576ebfbfe0a851962347c
Dima <poluhovicd601@gmail.com>
2024-12-21 17:39:27 +0200

    js file

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.navbar ul');
+async function getProducts() {
+    // Виконуємо запит до файлу "accesoires.json" та очікуємо на відповідь
+    let response = await fetch("accesories.json");
+    // Очікуємо на отримання та розпакування JSON-даних з відповіді
+    let products = await response.json();
+  
+    // Повертаємо отримані продукти
+    return products;
+  }
 
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
 
-const cartButtons = document.querySelectorAll('.add-to-cart');
+function card_html(product) {
+    return ` <div class="card">
+        <img src="images/${product.image}">
+        <div>
+            <h3>${product.name}</h3>
+            <p>${product.description}</p>
+            <p>${product.price}</p>
+            <a href="">Add to Cart</a>
+        </div>`
+}
 
-cartButtons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        event.preventDefault();
-        alert('Item added to cart!');
-    });
-});
+  getProducts().then(function(products){
+    let products_list = document.querySelector(".products-list")
+    if(products_list){
+        products.forEach(function(product){
+
+        })
+            products_list.innerHTML += card_html(product)
+        }});
